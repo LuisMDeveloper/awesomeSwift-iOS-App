63c105ddabe01d2c5b0937d7be43179fb170c371
Matteo Crippa <dev@matteocrippa.it>
2016-01-24 13:10:05 +0100

    first commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+use_frameworks!
+
+pod 'Parse'
+pod 'ParseUI'
+pod 'ImagePicker'
+pod 'DGElasticPullToRefresh', '~> 1.0'
+pod 'SwiftDate', '~> 1.2'
+pod 'JSONHelper', '~> 1.6'
+pod 'FontAwesome.swift', '~> 0.6'
---
+++ awesomeswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  awesomeswift
+//
+//  Created by Matteo Crippa on 24/01/2016.
+//  Copyright © 2016 boostco.de. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
