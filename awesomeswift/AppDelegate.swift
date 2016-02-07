bfce70c11ee1c46664b24f527677f990dbb11cf3
Matteo Crippa <dev@matteocrippa.it>
2016-02-07 12:56:46 +0100

    reset badge counter

--- awesomeswift/AppDelegate.swift
+++ awesomeswift/AppDelegate.swift
@@ -53,6 +53,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        // force reset badge counter
+        UIApplication.sharedApplication().applicationIconBadgeNumber = 0
+        
     }
 
     func applicationWillTerminate(application: UIApplication) {
