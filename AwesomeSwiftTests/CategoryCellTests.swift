c158fc9c85159c4dd551d3992318aea8cea6adab
Matteo Crippa <matteocrippa@users.noreply.github.com>
2016-03-30 00:52:05 +0200

    bdd for cell

--- AwesomeSwiftTests/CategoryCellTests.swift
+++ AwesomeSwiftTests/CategoryCellTests.swift
@@ -6,55 +6,59 @@
 //  Copyright © 2016 boostco.de. All rights reserved.
 //
 
-import XCTest
+import UIKit
+import Nimble
+import Quick
 @testable import AwesomeSwift
 
-class CategoryCellTests: XCTestCase {
+class CategoryCellTests: QuickSpec {
+    override func spec() {
 
-    var tableView: UITableView!
+        var cell: CategoryCell!
 
-   /* override func setUp() {
-        super.setUp()
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        // swiftlint:disable force_cast
-        let controller = storyboard.instantiateViewControllerWithIdentifier("CategoryListViewController") as! CategoryListViewController
-        _ = controller.view
+        var firstCat: CategoryModel!
 
-        tableView = controller.tableView
-        tableView.dataSource = FakeDataSource()
+        beforeEach() {
+            firstCat = CategoryModel()
+            firstCat.name = "name"
+
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            // swiftlint:disable force_cast
+            let vc = storyboard.instantiateViewControllerWithIdentifier("CategoryListViewController") as! CategoryListViewController
+            _ = vc.view
+            let tv = vc.tableView
+            let fakeDataSource = FakeDataSource()
+            tv.dataSource = fakeDataSource
+            // swiftlint:disable force_cast
+            cell = tv.dequeueReusableCellWithIdentifier(
+                "CategoryCell",
+                forIndexPath: NSIndexPath(forRow: 0, inSection: 0)
+            ) as! CategoryCell
+        }
+
+        describe("a category cell") {
+            it("has name label") {
+                expect(cell.textLabel).toNot(beNil())
+            }
+            it("sets cat name label") {
+                cell.configCellWithCategory(firstCat)
+                expect(cell.textLabel!.text).to(equal("name"))
+            }
+        }
     }
-
-    override func tearDown() {
-        super.tearDown()
-    }
-
-    func testSUT_HasNameLabel() {
-        // swiftlint:disable force_cast
-        let cell = tableView.dequeueReusableCellWithIdentifier("CategoryCell", forIndexPath: NSIndexPath(forRow: 0, inSection: 0)) as! CategoryCell
-
-        XCTAssertNotNil(cell.textLabel)
-    }
-
-    func testConfigWithCategory_SetsName() {
-        // swiftlint:disable force_cast
-        let cell = tableView.dequeueReusableCellWithIdentifier("CategoryCell", forIndexPath: NSIndexPath(forRow: 0, inSection: 0)) as! CategoryCell
-        let firstCat = CategoryModel()
-        firstCat.name = "test"
-        cell.configCellWithCategory(firstCat)
-        XCTAssertEqual(cell.textLabel?.text, "test")
-    }*/
-
 }
 
 extension CategoryCellTests {
     class FakeDataSource: NSObject, UITableViewDataSource {
+        func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+            return 1
+        }
 
         func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             return 1
         }
-
         func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            return UITableViewCell()
+            return CategoryCell()
         }
     }
 }
